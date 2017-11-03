e57c61bfa400e51b9029296a4832ca470fd5c0fb
Caglar Gonul <cgonul@netas.com.tr>
2017-11-03 15:52:54 +0300

    master Initial spring boot class.

---
+++ springboot/src/main/java/com/netas/poc/bpmn/WebAppExampleProcessApplication.java
@@ -0,0 +1,12 @@
+package com.netas.poc.bpmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppExampleProcessApplication {
+
+	public static void main(String... args) {
+		SpringApplication.run(WebAppExampleProcessApplication.class, args);
+	}
+}
