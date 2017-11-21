e42d5d03e319497acb92a256ad9c9a0f226b4a81
Caglar Gonul <cgonul@netas.com.tr>
2017-11-21 18:08:08 +0300

    master Deploying an assigned model is implemented.

--- springboot/src/main/java/com/netas/poc/bpmn/WebAppExampleProcessApplication.java
+++ springboot/src/main/java/com/netas/poc/bpmn/WebAppExampleProcessApplication.java
@@ -1,12 +1,26 @@
 package com.netas.poc.bpmn;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class WebAppExampleProcessApplication {
+
+	@Autowired
+	private RuntimeService runtimeService;
 
 	public static void main(String... args) {
 		SpringApplication.run(WebAppExampleProcessApplication.class, args);
+	}
+
+	@EventListener
+	private void processPostDeploy(PostDeployEvent event) {
+		runtimeService.startProcessInstanceByKey("loanApproval");
 	}
 }
